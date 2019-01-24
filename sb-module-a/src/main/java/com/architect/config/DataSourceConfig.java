75db47255307d31fb03bcf28b119d0944b631f3e
加文雄 <jiawenxiong@newbeescm.com>
2019-01-24 14:25:52 +0800

    优化代码

--- sb-module-a/src/main/java/com/architect/config/DataSourceConfig.java
+++ sb-module-a/src/main/java/com/architect/config/DataSourceConfig.java
@@ -1,96 +1,96 @@
-//package com.architect.config;
-//
-//import com.architect.algorithm.DemoDatabaseShardingAlgorithm;
-//import com.architect.algorithm.DemoTableShardingAlgorithm;
-//import io.shardingjdbc.core.api.config.ShardingRuleConfiguration;
-//import io.shardingjdbc.core.api.config.TableRuleConfiguration;
-//import io.shardingjdbc.core.api.config.strategy.StandardShardingStrategyConfiguration;
-//import io.shardingjdbc.core.jdbc.core.datasource.ShardingDataSource;
-//import org.apache.commons.dbcp.BasicDataSource;
-//import org.apache.ibatis.session.SqlSessionFactory;
-//import org.mybatis.spring.SqlSessionFactoryBean;
-//import org.mybatis.spring.SqlSessionTemplate;
-//import org.mybatis.spring.annotation.MapperScan;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Primary;
-//import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-//import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-//
-//import javax.sql.DataSource;
-//import java.sql.SQLException;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-///**
-// * @author wenxiong.jia
-// * @since 2018/8/25
-// */
-////@Configuration
-////@MapperScan(basePackages = "com.architect.dao", sqlSessionTemplateRef = "testSqlSessionTemplate")
-//public class DataSourceConfig {
-//
-//    /**
-//     * 配置分库分表策略
-//     */
-//    @Bean(name = "shardingDataSource")
-//    DataSource getShardingDataSource() throws SQLException {
-//        ShardingRuleConfiguration shardingRuleConfig = new ShardingRuleConfiguration();
-//        shardingRuleConfig.getTableRuleConfigs().add(getUserTableRuleConfiguration());
-//        shardingRuleConfig.getBindingTableGroups().add("t_user");
-//        shardingRuleConfig.setDefaultDatabaseShardingStrategyConfig(new StandardShardingStrategyConfiguration("id", DemoDatabaseShardingAlgorithm.class.getName()));
-//        shardingRuleConfig.setDefaultTableShardingStrategyConfig(new StandardShardingStrategyConfiguration("id", DemoTableShardingAlgorithm.class.getName()));
-//        return new ShardingDataSource(shardingRuleConfig.build(createDataSourceMap()));
-//    }
-//
-//    /**
-//     * 设置表的节点
-//     */
-//    @Bean
-//    TableRuleConfiguration getUserTableRuleConfiguration() {
-//        TableRuleConfiguration tableRuleConfiguration = new TableRuleConfiguration();
-//        tableRuleConfiguration.setLogicTable("t_user");
-//        tableRuleConfiguration.setActualDataNodes("architect${0..1}.t_user${0..1}");
-//        tableRuleConfiguration.setKeyGeneratorColumnName("id");
-//        return tableRuleConfiguration;
-//    }
-//
-//    /**
-//     * 需要手动配置事务管理器
-//     */
-//    @Bean
-//    public DataSourceTransactionManager transactitonManager(DataSource shardingDataSource) {
-//        return new DataSourceTransactionManager(shardingDataSource);
-//    }
-//
-//    @Bean
-//    @Primary
-//    public SqlSessionFactory sqlSessionFactory(DataSource shardingDataSource) throws Exception {
-//        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
-//        bean.setDataSource(shardingDataSource);
-//        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml"));
-//        return bean.getObject();
-//    }
-//
-//    @Bean
-//    @Primary
-//    public SqlSessionTemplate testSqlSessionTemplate(SqlSessionFactory sqlSessionFactory) throws Exception {
-//        return new SqlSessionTemplate(sqlSessionFactory);
-//    }
-//
-//    private Map<String, DataSource> createDataSourceMap() {
-//        Map<String, DataSource> result = new HashMap<>(2);
-//        result.put("architect0", createDataSource("architect0"));
-//        result.put("architect1", createDataSource("architect1"));
-//        return result;
-//    }
-//
-//    private DataSource createDataSource(String dataSourceName) {
-//        BasicDataSource dataSource = new BasicDataSource();
-//        dataSource.setDriverClassName(com.mysql.jdbc.Driver.class.getName());
-//        dataSource.setUrl(String.format("jdbc:mysql://localhost:3306/%s", dataSourceName));
-//        dataSource.setUsername("root");
-//        dataSource.setPassword("root");
-//        return dataSource;
-//    }
-//}
+package com.architect.config;
+
+import com.architect.algorithm.DemoDatabaseShardingAlgorithm;
+import com.architect.algorithm.DemoTableShardingAlgorithm;
+import io.shardingjdbc.core.api.config.ShardingRuleConfiguration;
+import io.shardingjdbc.core.api.config.TableRuleConfiguration;
+import io.shardingjdbc.core.api.config.strategy.StandardShardingStrategyConfiguration;
+import io.shardingjdbc.core.jdbc.core.datasource.ShardingDataSource;
+import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author wenxiong.jia
+ * @since 2018/8/25
+ */
+@Configuration
+@MapperScan(basePackages = "com.architect.dao", sqlSessionTemplateRef = "testSqlSessionTemplate")
+public class DataSourceConfig {
+
+    /**
+     * 配置分库分表策略
+     */
+    @Bean(name = "shardingDataSource")
+    DataSource getShardingDataSource() throws SQLException {
+        ShardingRuleConfiguration shardingRuleConfig = new ShardingRuleConfiguration();
+        shardingRuleConfig.getTableRuleConfigs().add(getUserTableRuleConfiguration());
+        shardingRuleConfig.getBindingTableGroups().add("t_user");
+        shardingRuleConfig.setDefaultDatabaseShardingStrategyConfig(new StandardShardingStrategyConfiguration("id", DemoDatabaseShardingAlgorithm.class.getName()));
+        shardingRuleConfig.setDefaultTableShardingStrategyConfig(new StandardShardingStrategyConfiguration("id", DemoTableShardingAlgorithm.class.getName()));
+        return new ShardingDataSource(shardingRuleConfig.build(createDataSourceMap()));
+    }
+
+    /**
+     * 设置表的节点
+     */
+    @Bean
+    TableRuleConfiguration getUserTableRuleConfiguration() {
+        TableRuleConfiguration tableRuleConfiguration = new TableRuleConfiguration();
+        tableRuleConfiguration.setLogicTable("t_user");
+        tableRuleConfiguration.setActualDataNodes("architect${0..1}.t_user${0..1}");
+        tableRuleConfiguration.setKeyGeneratorColumnName("id");
+        return tableRuleConfiguration;
+    }
+
+    /**
+     * 需要手动配置事务管理器
+     */
+    @Bean
+    public DataSourceTransactionManager transactitonManager(DataSource shardingDataSource) {
+        return new DataSourceTransactionManager(shardingDataSource);
+    }
+
+    @Bean
+    @Primary
+    public SqlSessionFactory sqlSessionFactory(DataSource shardingDataSource) throws Exception {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(shardingDataSource);
+        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml"));
+        return bean.getObject();
+    }
+
+    @Bean
+    @Primary
+    public SqlSessionTemplate testSqlSessionTemplate(SqlSessionFactory sqlSessionFactory) throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+    private Map<String, DataSource> createDataSourceMap() {
+        Map<String, DataSource> result = new HashMap<>(2);
+        result.put("architect0", createDataSource("architect0"));
+        result.put("architect1", createDataSource("architect1"));
+        return result;
+    }
+
+    private DataSource createDataSource(String dataSourceName) {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(com.mysql.jdbc.Driver.class.getName());
+        dataSource.setUrl(String.format("jdbc:mysql://localhost:3306/%s", dataSourceName));
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        return dataSource;
+    }
+}
